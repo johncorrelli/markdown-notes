8f67c37d11a612f3daa622dd52af67c1134e891f
John Correlli <john@teamgantt.com>
2019-06-14 16:25:42 -0400

    Groups notes by category

---
+++ src/helpers/categories.js
@@ -0,0 +1,36 @@
+export const getNoteCategories = notes => {
+  const uniqueCategories = [];
+
+  notes.forEach(note => {
+    const {category} = note;
+
+    if (uniqueCategories.indexOf(category) !== -1) {
+      return;
+    }
+
+    uniqueCategories.push(category);
+  });
+
+  uniqueCategories.sort((a, b) => {
+    const aCategory = a.toLowerCase();
+    const bCategory = b.toLowerCase();
+
+    if (aCategory < bCategory) {
+      return -1;
+    }
+    if (aCategory > bCategory) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return uniqueCategories;
+};
+
+export const getNotesByCategory = (notes, selectedCategory) => {
+  if (!selectedCategory) {
+    return notes;
+  }
+
+  return notes.filter(note => note.category === selectedCategory);
+};
